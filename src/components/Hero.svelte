dc0177ef3a99298d758b65a23aac650300ed0013
Lautaro Amengual <amengual.ln@gmail.com>
2024-06-02 14:23:26 -0300

    Fix relative paths on assets

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,5 +1,5 @@
 <section
-  class="min-h-[80vh] bg-[#9a9a9a] bg-[url('landing-mobile.png')] md:bg-[url('landing.png')] bg-[-30vw_120px] md:bg-center bg-no-repeat"
+  class="min-h-[80vh] bg-[#9a9a9a] bg-[url('/landing-mobile.png')] md:bg-[url('/landing.png')] bg-[-30vw_120px] md:bg-center bg-no-repeat"
 >
   <div class="grid gap-4 md:grid-cols-2 md:items-center md:min-h-[80vh] md:max-w-[70vw] mx-auto text-center md:text-xl p-8">
     <div class="grid gap-4">
