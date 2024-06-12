7bd3f8ae37dea4fdd83e009f42b2e35e9bdda395
Lautaro Amengual <amengual.ln@gmail.com>
2024-06-12 17:35:14 -0300

    Center CEO de tu vida text

--- src/components/CEODeTuVida.svelte
+++ src/components/CEODeTuVida.svelte
@@ -4,23 +4,23 @@
       <h2 class="font-[600] text-center pb-4">
         ¿Qué significa ser CEO de tu Vida?
       </h2>
-      <p>
+      <p class="text-center lg:text-left">
         Tu proyecto más importante no es tu trabajo, ni tu
         maternidad/paternidad, ni tu relación de pareja. Tu proyecto más
         importante es TU VIDA.
       </p>
-      <p>
+      <p class="text-center lg:text-left">
         Por lo tanto, si quieres tener una vida altamente rentable (en dinero,
         amor, reconocimiento, felicidad, paz mental, salud, etc.), es importante
         que te transformes en el CEO de tu vida y aprendas a utilizar tus
         herramientas más valiosas: tu cerebro, tu mente y tu cuerpo.
       </p>
-      <p>
+      <p class="text-center lg:text-left">
         Así que antes de liderar un proyecto, un equipo, una organización o un
         hogar, es crucial que empieces por liderar tu propia vida con maestría.
       </p>
       <h2 class="font-[600] text-center pb-4 mt-12">¿Por qué es importante?</h2>
-      <p>
+      <p class="text-center lg:text-left">
         Si al día de hoy no estás satisfecho/a con los resultados que has
         obtenido en lo personal y/o en lo profesional, muy probablemente se deba
         a que llevas años pensando como la abuelita de tu abuelita, es decir,
@@ -28,13 +28,13 @@
         corriendo detrás de un objetivo de éxito y felicidad que nada tiene que
         ver contigo.
       </p>
-      <p>
+      <p class="text-center lg:text-left">
         Si quieres tener una vida EXTRA-ordinaria y lograr tus metas más
         ambiciosas, es momento de romper ese ciclo y recablear tu mente para que
         pienses, decidas y actúes según tus propias reglas.
       </p>
       <h2 class="font-[600] text-center pb-4 mt-12">¿Qué ofrecemos?</h2>
-      <p>
+      <p class="text-center lg:text-left">
         CEO de tu Vida es un programa exclusivo de coaching diseñado para
         líderes y visionarios como tú, que buscan transformar su vida personal y
         profesional. Ofrecemos tres formatos únicos para ayudarte a alcanzar tu
