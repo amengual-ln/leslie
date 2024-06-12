4f2abbabf29e1449279c94f59e61003eefe94c09
Lautaro Amengual <amengual.ln@gmail.com>
2024-06-12 17:32:14 -0300

    Whatsapp widget

---
+++ src/components/WhatsappWidget.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Whatsapp from "./icons/Whatsapp.svelte";
+</script>
+
+<div
+  class="fixed bottom-0 right-0 m-8 drop-shadow-lg opacity-50 hover:opacity-100 cursor-pointer z-50"
+>
+  <a href="https://walink.co/ba9b16" target="_blank" rel="noopener noreferrer">
+    <Whatsapp />
+  </a>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,13 @@
 <script>
     import Header from "../components/Header.svelte";
     import Footer from "../components/Footer.svelte";
+    import WhatsappWidget from "../components/WhatsappWidget.svelte";
     import "../app.css";
 </script>
 
 <Header />
 
 <slot />
+<WhatsappWidget />
 
 <Footer />
