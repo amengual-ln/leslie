b28a5bc0db8600d70d9ef3808c75fc75b056ca70
techloudintelligence <maria.carbajal@loudintelligence.com>
2024-06-29 23:22:00 +0200

    Benefits section

--- src/components/CEODeTuVida.svelte
+++ src/components/CEODeTuVida.svelte
@@ -1,9 +1,13 @@
+<script>
+  import Button from "./atoms/Button.svelte";
+</script>
+
 <section id="ceo-de-tu-vida" class="text-xl p-4">
   <div class="flex gap-12 px-4 py-12">
     <div class="flex flex-col justify-center flex-1">
-      <h2 class="font-[600] text-center pb-4">
+      <h1 class="font-[600] text-center pb-4">
         ¿Qué significa ser CEO de tu Vida?
-      </h2>
+      </h1>
       <p class="text-center lg:text-left">
         Tu proyecto más importante no es tu trabajo, ni tu
         maternidad/paternidad, ni tu relación de pareja. Tu proyecto más
@@ -19,7 +23,7 @@
         Así que antes de liderar un proyecto, un equipo, una organización o un
         hogar, es crucial que empieces por liderar tu propia vida con maestría.
       </p>
-      <h2 class="font-[600] text-center pb-4 mt-12">¿Por qué es importante?</h2>
+      <h1 class="font-[600] text-center pb-4 mt-12">¿Por qué es importante?</h1>
       <p class="text-center lg:text-left">
         Si al día de hoy no estás satisfecho/a con los resultados que has
         obtenido en lo personal y/o en lo profesional, muy probablemente se deba
@@ -33,7 +37,7 @@
         ambiciosas, es momento de romper ese ciclo y recablear tu mente para que
         pienses, decidas y actúes según tus propias reglas.
       </p>
-      <h2 class="font-[600] text-center pb-4 mt-12">¿Qué ofrecemos?</h2>
+      <h1 class="font-[600] text-center pb-4 mt-12">¿Qué ofrecemos?</h1>
       <p class="text-center lg:text-left">
         CEO de tu Vida es un programa exclusivo de coaching diseñado para
         líderes y visionarios como tú, que buscan transformar su vida personal y
@@ -41,5 +45,10 @@
         boy y te transformes en CEO de tu vida:
       </p>
     </div>
+  </div>
+  <div class="grid justify-center">
+    <a href="/benefits" class="mt-12">
+      <Button value="Conoce más..." />
+    </a>
   </div>
 </section>
---
+++ src/routes/benefits/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+    import Button from "../../components/atoms/Button.svelte";
+</script>
+
+  <section id="benefits" class="text-xl p-4">
+    <div class="flex gap-12 px-4 py-12">
+      <div class="flex flex-col justify-center flex-1">
+        <h2 class="font-[600] text-center pb-4 mt-12">Transformación Total: Conviértete en el CEO de tu Vida</h2>
+        <p class="text-center lg:text-left">
+          El objetivo principal de transformarte en el CEO de tu vida es que tengas una Vida Altamente Rentable, y los resultados que obtendrás, son:
+        </p>
+        <ul class="list-disc pl-8 text-left">
+          <li><em>Romper Barreras Internas</em>: Destruye esos techos de cristal que limitan y sabotean tu carrera profesional y tu vida personal.</li>
+          <li><em>Aumentar tus Ingresos</em>: Muchos de mis clientes han triplicado sus ingresos mientras reducen sus horas de trabajo. Gana más dinero trabajando menos.</li>
+          <li><em>Desarrollar una Mentalidad a prueba de crisis</em>: Afronta crisis emocionales, financieras y profesionales con una mentalidad resiliente y segura.</li>
+          <li><em>Liderar equipos felices y altamente productivos</em>: Lleva a tu equipo a alcanzar mejores resultados sin agotarte ni convertirte en un capataz, convirtiéndote en un líder que inspira e impacta positivamente.</li>
+          <li><em>Alcanzar Metas Ambiciosas</em>: Logra objetivos profesionales más grandes y ambiciosos sin sacrificar tu paz mental.</li>
+          <li><em>Sentirte Pleno y Satisfecho</em>: Experimenta una mayor felicidad y satisfacción tanto en tu vida personal como profesional.</li>
+          <li><em>Recuperar tu Energía</em>: Desarrolla una rutina de bienestar que te mantenga con vitalidad y energía para vivir cada día con entusiasmo.</li>
+          <li><em>Disfrutar más con tu Familia</em>: Pasa más tiempo de calidad con tus seres queridos sin bajar tu rendimiento profesional.</li>
+          <li><em>Desconectarte sin Culpa</em>: Aprende a desconectarte del trabajo sin sentir culpa, mientras sigues dando grandes resultados en tu puesto.</li>
+          <li><em>Superar el Síndrome del Impostor</em>: Libérate del síndrome del impostor y vive con ligereza, en constante expansión y crecimiento.</li>
+        </ul>
+        <p class="text-center lg:text-left mt-4">
+          ¡Es tu momento de tomar el control y convertirte en el CEO de tu vida!
+        </p>
+      </div>
+    </div>
+    <div class="grid justify-center">
+        <a href="https://walink.co/bccb88" target="_blank" rel="noopener noreferrer" class="my-8">
+          <Button value="¿Estas listo para tomar el control?" />
+        </a>
+    </div>
+  </section>
+  
