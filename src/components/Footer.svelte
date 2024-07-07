5a6ceded6e5c1cc632c5d39fc0d2c54f36639404
Lautaro Amengual <amengual.ln@gmail.com>
2024-07-06 22:41:09 -0300

    Fix newsletter form styles. Page title.

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -6,9 +6,13 @@
   import Youtube from "./icons/Youtube.svelte";
 </script>
 
-<footer id="contacto" class="w-full bg-black text-xl text-white text-center px-4">
+<footer
+  id="contacto"
+  class="w-full bg-black text-xl text-white text-center px-4"
+>
   <section
-    class="max-w-screen-2xl mx-auto p-8 grid grid-cols-1 lg:grid-cols-[2fr_1fr] lg:items-center gap-4 lg:gap-8 justify-center">
+    class="max-w-screen-2xl mx-auto p-8 grid grid-cols-1 lg:grid-cols-[2fr_1fr] lg:items-center gap-4 lg:gap-8 justify-center"
+  >
     <div class="grid justify-center gap-4 lg:gap-8 order-last lg:order-first">
       <nav class="order-first">
         <ul class="gap-4 hidden lg:flex">
@@ -20,66 +24,105 @@
         </ul>
       </nav>
       <div class="order-last lg:order-first">
-        <img src="/logo.png" alt="Leslie Beltrán" class="h-20 lg:h-32 mx-auto" />
+        <img
+          src="/logo.png"
+          alt="Leslie Beltrán"
+          class="h-20 lg:h-32 mx-auto"
+        />
         <span class="text-sm opacity-80">MINDSET FOR SUCCESS</span>
       </div>
       <ul class="flex justify-center gap-8 mt-2">
         <li class="opacity-75 hover:opacity-100">
-          <a href="https://www.facebook.com/leslie.bienestar" target="_blank" rel="noopener noreferrer"><Facebook /></a>
+          <a
+            href="https://www.facebook.com/leslie.bienestar"
+            target="_blank"
+            rel="noopener noreferrer"><Facebook /></a
+          >
         </li>
         <li class="opacity-75 hover:opacity-100">
-          <a href="https://www.instagram.com/lesliebeltran.oficial/" target="_blank" rel="noopener noreferrer"><Instagram /></a>
+          <a
+            href="https://www.instagram.com/lesliebeltran.oficial/"
+            target="_blank"
+            rel="noopener noreferrer"><Instagram /></a
+          >
         </li>
         <li class="opacity-75 hover:opacity-100">
-          <a href="https://www.linkedin.com/in/leslie-beltr%C3%A1n-%F0%9F%87%B2%F0%9F%87%BD-872267130/" target="_blank"
-            rel="noopener noreferrer"><Linkedin /></a>
+          <a
+            href="https://www.linkedin.com/in/leslie-beltr%C3%A1n-%F0%9F%87%B2%F0%9F%87%BD-872267130/"
+            target="_blank"
+            rel="noopener noreferrer"><Linkedin /></a
+          >
         </li>
         <li class="opacity-75 hover:opacity-100">
-          <a href="https://www.youtube.com/channel/UCxKftN2Ia_DKYROt13GOS-A" target="_blank" rel="noopener noreferrer"><Youtube /></a>
+          <a
+            href="https://www.youtube.com/channel/UCxKftN2Ia_DKYROt13GOS-A"
+            target="_blank"
+            rel="noopener noreferrer"><Youtube /></a
+          >
         </li>
       </ul>
     </div>
-    <div id="mlb2-16173497" class="ml-form-embedContainer ml-subscribe-form ml-subscribe-form-16173497">
+    <div
+      id="mlb2-16173497"
+      class="ml-form-embedContainer ml-subscribe-form ml-subscribe-form-16173497"
+    >
       <div class="ml-form-align-center">
         <div class="ml-form-embedWrapper embedForm">
           <div class="ml-form-embedBody ml-form-embedBodyDefault row-form">
-            <div class="ml-form-embedContent">
+            <div class="ml-form-embedContent text-center mb-4">
               <p>¡Suscríbete a mi Newsletter para recibir mi contenido!</p>
             </div>
-            <form class="ml-block-form" action="https://assets.mailerlite.com/jsonp/780653/forms/125961743657600458/subscribe"
-              data-code="" method="post" target="_blank">
+            <form
+              class="ml-block-form"
+              action="https://assets.mailerlite.com/jsonp/780653/forms/125961743657600458/subscribe"
+              data-code=""
+              method="post"
+              target="_blank"
+            >
               <div class="ml-form-formContent">
                 <div class="ml-form-fieldRow ml-last-item">
-                  <div class="ml-field-group ml-field-email ml-validate-email ml-validate-required">
-                    <input aria-label="email" aria-required="true" type="email" class="form-control" data-inputmask=""
-                      name="fields[email]" placeholder="Email" autocomplete="email">
+                  <div
+                    class="ml-field-group ml-field-email ml-validate-email ml-validate-required"
+                  >
+                    <input
+                      aria-label="email"
+                      aria-required="true"
+                      type="email"
+                      class="form-control mb-4"
+                      data-inputmask=""
+                      name="fields[email]"
+                      placeholder="Email"
+                      autocomplete="email"
+                    />
                   </div>
                 </div>
               </div>
-              <div class="ml-form-embedPermissions">
-              </div>
+              <div class="ml-form-embedPermissions"></div>
               <div class="ml-form-checkboxRow ml-validate-required">
-                <label class="checkbox">
-                  <input type="checkbox">
-                  <div class="label-description">
-                    <p>Acepto la Política de Privacidad</p>
-                  </div>
+                <label class="checkbox flex items-center gap-4 mb-6 cursor-pointer">
+                  <input type="checkbox" class="text-center" />
+                  <p>Acepto la Política de Privacidad</p>
                 </label>
               </div>
-              <input type="hidden" name="ml-submit" value="1">
+              <input type="hidden" name="ml-submit" value="1" />
               <div class="ml-form-embedSubmit">
                 <button type="submit" class="primary">Subscribirse</button>
-                <button disabled="disabled" style="display: none;" type="button" class="loading">
+                <button
+                  disabled="disabled"
+                  style="display: none;"
+                  type="button"
+                  class="loading"
+                >
                   <div class="ml-form-embedSubmitLoad"></div>
                   <span class="sr-only">Loading...</span>
                 </button>
               </div>
-              <input type="hidden" name="anticsrf" value="true">
+              <input type="hidden" name="anticsrf" value="true" />
             </form>
           </div>
           <div class="ml-form-successBody row-success" style="display: none">
             <div class="ml-form-successContent">
-              <h3>¡Gracias!</h3>
+              <h3 class="mb-4">¡Gracias!</h3>
               <p>Te has suscrito con éxito.</p>
             </div>
           </div>
@@ -89,18 +132,24 @@
     <script>
       function ml_webform_success_16173497() {
         var $ = ml_jQuery || jQuery;
-        $('.ml-subscribe-form-16173497 .row-success').show();
-        $('.ml-subscribe-form-16173497 .row-form').hide();
+        $(".ml-subscribe-form-16173497 .row-success").show();
+        $(".ml-subscribe-form-16173497 .row-form").hide();
       }
     </script>
-    <script src="https://groot.mailerlite.com/js/w/webforms.min.js?v2d8fb22bb5b3677f161552cd9e774127" type="text/javascript"></script>
+    <script
+      src="https://groot.mailerlite.com/js/w/webforms.min.js?v2d8fb22bb5b3677f161552cd9e774127"
+      type="text/javascript"
+    ></script>
     <script>
-      fetch("https://assets.mailerlite.com/jsonp/780653/forms/125961743657600458/takel")
+      fetch(
+        "https://assets.mailerlite.com/jsonp/780653/forms/125961743657600458/takel",
+      );
     </script>
   </section>
 </footer>
 
-<style type="text/css">@import url("https://assets.mlcdn.com/fonts.css?version=1719836");
+<style type="text/css">
+  @import url("https://assets.mlcdn.com/fonts.css?version=1719836");
   /* LOADER */
   .ml-form-embedSubmitLoad {
     display: inline-block;
@@ -123,7 +172,7 @@
     padding: 0;
     margin: -1px;
     overflow: hidden;
-    clip: rect(0,0,0,0);
+    clip: rect(0, 0, 0, 0);
     border: 0;
   }
 
@@ -169,7 +218,6 @@
     background-color: #141414;
     border-width: 0px;
     border-color: transparent;
-    border-radius: 4px;
     border-style: solid;
     box-sizing: border-box;
     display: inline-block !important;
@@ -208,24 +256,32 @@
     text-align: right;
   }
 
-  #mlb2-16173497.ml-form-embedContainer .ml-form-embedWrapper .ml-form-embedHeader img {
-    border-top-left-radius: 4px;
-    border-top-right-radius: 4px;
+  #mlb2-16173497.ml-form-embedContainer
+    .ml-form-embedWrapper
+    .ml-form-embedHeader
+    img {
     height: auto;
     margin: 0 auto !important;
     max-width: 100%;
   }
 
-  #mlb2-16173497.ml-form-embedContainer .ml-form-embedWrapper .ml-form-embedBody,
+  #mlb2-16173497.ml-form-embedContainer
+    .ml-form-embedWrapper
+    .ml-form-embedBody,
   #mlb2-16173497.ml-form-embedContainer .ml-form-successBody {
     padding: 20px 20px 0 20px;
   }
 
-  #mlb2-16173497.ml-form-embedContainer .ml-form-embedWrapper .ml-form-embedBody.ml-form-embedBodyHorizontal {
+  #mlb2-16173497.ml-form-embedContainer
+    .ml-form-embedWrapper
+    .ml-form-embedBody.ml-form-embedBodyHorizontal {
     padding-bottom: 0;
   }
 
-  #mlb2-16173497.ml-form-embedContainer .ml-form-embedWrapper .ml-form-embedBody .ml-form-embedContent,
+  #mlb2-16173497.ml-form-embedContainer
+    .ml-form-embedWrapper
+    .ml-form-embedBody
+    .ml-form-embedContent,
   #mlb2-16173497.ml-form-embedContainer .ml-form-successBody .ml {
     padding-bottom: 0;
   }
@@ -236,8 +292,7 @@
     padding: 12px 24px;
     border: none;
     cursor: pointer;
-    font-family: 'Poppins', sans-serif;
-    border-radius: 4px;
+    font-family: "Poppins", sans-serif;
     transition: background-color 0.3s ease;
   }
 
@@ -246,14 +301,14 @@
   }
 
   h4 {
-    font-family: 'Baskerville', serif;
+    font-family: "Baskerville", serif;
   }
 
   p,
   input,
   label,
   button {
-    font-family: 'Poppins', sans-serif;
+    font-family: "Poppins", sans-serif;
   }
 
   p {
@@ -263,8 +318,7 @@
   input {
     padding: 10px;
     border: none;
-    border-radius: 4px;
-    margin-bottom: 10px;
+    color: black;
   }
 
   .ml-form-embedBodyDefault {
