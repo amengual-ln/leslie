6640b6122775720d79423c801f79e891285fd29e
María Carbajal <mcarbmont89@users.noreply.github.com>
2024-07-19 23:34:04 +0200

    CEO Test

--- src/components/Blog.svelte
+++ src/components/Blog.svelte
@@ -12,53 +12,15 @@
       <article class="bg-[#222] m-4 p-4 text-white flex-1 flex flex-col gap-4">
         <img src="blog1.png" alt="Imagen del articulo" />
         <p class="py-2 text-justify text-balance">
-          Que las circunstancias, un “mal jefe”, una mala racha, tu agenda o tu
-          pareja NO controlen tu vida o decidan tu estado de ánimo. Ante
-          cualquier situación tu siempre tienes el poder de elegir que actitud
-          tomar. Elige siempre lo que te dé paz.
+          CEO Test Da el primer paso para transformarte en CEO de tu vida!El CEO Test te permitirá identificar si eres el CEO de tu vida o el Office Boy. Responde el CEO TEST y te regalo una sesión individual conmigo para conversar sobre tus resultados y generar una estrategia que te ayude a transformarte en CEO de tu vida. Precio Especial:$39 USD Adquiérelo Ahora
         </p>
         <div class="text-center pt-4">
           <a
-            href="https://lesliebeltran.mx/blog/"
+            href="https://walink.co/bccb88"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Button value="Leer más" />
-          </a>
-        </div>
-      </article>
-      <article class="bg-[#222] m-4 p-4 text-white flex-1 flex flex-col gap-4">
-        <img src="blog2.jpeg" alt="Imagen del articulo" />
-        <p class="py-2 text-justify text-balance">
-          Que tu pasado o las etiquetas que te han impuesto NO te definan. Tú
-          vas a ser quien elijas ser a partir de hoy.
-        </p>
-        <div class="text-center pt-4">
-          <a
-            href="https://lesliebeltran.mx/blog/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Button value="Leer más" />
-          </a>
-        </div>
-      </article>
-      <article class="bg-[#222] m-4 p-4 text-white flex-1 flex flex-col gap-4">
-        <img src="blog3.jpeg" alt="Imagen del articulo" />
-        <p class="py-2 text-justify text-balance">
-          Que tus emociones no secuestren tu objetividad y capacidad de
-          decisión. La inteligencia emocional es una capacidad que se puede
-          desarrollar. Es normal y sano sentir enojo, frustración, tristeza o
-          miedo, pero no te funciona tomar decisiones definitivas cuando
-          transitas por emociones temporales.
-        </p>
-        <div class="text-center pt-4">
-          <a
-            href="https://lesliebeltran.mx/blog/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Button value="Leer más" />
+            <Button value="Quiero hacer el CEO Test" />
           </a>
         </div>
       </article>
