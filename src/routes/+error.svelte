260fcabc48f81c3962a073840f3008312245cf23
Lautaro Amengual <amengual.ln@gmail.com>
2025-01-29 22:27:20 -0300

    custom 404

---
+++ src/routes/+error.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Button from "../components/atoms/Button.svelte";
+</script>
+
+<section class="min-h-[60vh] flex items-center justify-center">
+  <div class="grid justify-center text-center gap-8">
+    <h1 class="font-bold text-[64px]">404</h1>
+    <h2 class="text-[24px]">Aqui no hay nada</h2>
+    <a href="/">
+      <Button value="Inicio" />
+    </a>
+  </div>
+</section>
