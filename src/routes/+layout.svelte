de33cdfeb10205f3206ec4c0509122e9ea910be6
María Carbajal <mcarbmont89@users.noreply.github.com>
2025-09-16 10:44:25 +0200

    Update +layout.svelte Pixel FB

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,30 @@
 <script>
-    import Header from "../components/Header.svelte";
-    import Footer from "../components/Footer.svelte";
-    import WhatsappWidget from "../components/WhatsappWidget.svelte";
-    import "../app.css";
+  import Header from "../components/Header.svelte";
+  import Footer from "../components/Footer.svelte";
+  import WhatsappWidget from "../components/WhatsappWidget.svelte";
+  import "../app.css";
 </script>
+
+<svelte:head>
+  <!-- Meta Pixel Code -->
+  <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '678805428456499');
+    fbq('track', 'PageView');
+  </script>
+  <noscript>
+    <img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=678805428456499&ev=PageView&noscript=1"/>
+  </noscript>
+  <!-- End Meta Pixel Code -->
+</svelte:head>
 
 <Header />
 
