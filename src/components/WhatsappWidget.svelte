4f224aa400b04ed2979aa82f217e02eeac79b003
María Carbajal <mcarbmont89@users.noreply.github.com>
2026-01-12 00:54:25 +0100

    Update WhatsApp link in WhatsappWidget component

--- src/components/WhatsappWidget.svelte
+++ src/components/WhatsappWidget.svelte
@@ -5,7 +5,7 @@
 <div
   class="fixed bottom-0 right-0 m-8 drop-shadow-lg opacity-50 hover:opacity-100 cursor-pointer z-50"
 >
-  <a href="https://walink.co/45aabc" target="_blank" rel="noopener noreferrer">
+  <a href="https://wa.link/ceodetuvidasesiones" target="_blank" rel="noopener noreferrer">
     <Whatsapp />
   </a>
 </div>
